202b859f84fe103d5b071b06b2c725b64ed956a2
honsy <a250534119@outlook.com>
2025-05-30 01:40:40 +0800

    fix bug

--- build/services/notify.js
+++ build/services/notify.js
@@ -99,7 +99,7 @@ let NotificationService = class NotificationService {
                 const digit = match[1];
                 priority = Number(digit);
             }
-            const res = await http_1.httpClient.post(`${gotifyUrl}/message?token=${gotifyToken}`, Object.assign(Object.assign({}, this.gotOption), { body: `title=${encodeURIComponent(this.title.replace(regex))}&message=${encodeURIComponent(this.content)}&priority=${priority}`, headers: {
+            const res = await http_1.httpClient.post(`${gotifyUrl}/message?token=${gotifyToken}`, Object.assign(Object.assign({}, this.gotOption), { body: `title=${encodeURIComponent(this.title.replace(regex,''))}&message=${encodeURIComponent(this.content)}&priority=${priority}`, headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 } }));
             if (typeof res.id === 'number') {
